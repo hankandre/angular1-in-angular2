8763e28c433f8d3745b25aefb5bc0f318567adc6
Hank Andre <hankandre@gmail.com>
2016-11-18 22:16:13 -0600

    simulating immutable state in Angular 1

--- client/app/components/bookmarks/bookmarks.controller.js
+++ client/app/components/bookmarks/bookmarks.controller.js
@@ -1,16 +1,54 @@
-class BookmarksController{
-    constructor(BookmarksModel, CategoriesModel){
-        'ngInject';
-        this.BookmarksModel = BookmarksModel;
-        this.CategoriesModel = CategoriesModel;
-    }
+class BookmarksController {
+  constructor(CategoriesModel, BookmarksModel) {
+    'ngInject';
 
-    $onInit() {
-        this.BookmarksModel.getBookmarks()
-            .then(result => this.bookmarks = result);
-        
-        this.getCurrentCategory = this.CategoriesModel.getCurrentCategory.bind(this.CategoriesModel)
+    this.CategoriesModel = CategoriesModel;
+    this.BookmarksModel = BookmarksModel;
+  }
+
+  $onInit() {
+    this.BookmarksModel.getBookmarks()
+      .then((bookmarks) => {
+        this.bookmarks = bookmarks;
+      });
+
+    this.getCurrentCategory = this.CategoriesModel.getCurrentCategory.bind(this.CategoriesModel);
+    this.deleteBookmark = this.BookmarksModel.deleteBookmark;
+  }
+
+  createBookmark() {
+    this.currentBookmark = this.initNewBookmark();
+  }
+
+  editBookmark(bookmark) {
+    this.currentBookmark = bookmark;
+  }
+
+  initNewBookmark() {
+    return {
+      id: null,
+      title: '',
+      url: '',
+      category: this.CategoriesModel.getCurrentCategory().name
+    };
+  }
+
+  saveBookmark(bookmark) {
+    if (bookmark.id) {
+      this.BookmarksModel.updateBookmark(bookmark);
+    } else {
+      this.BookmarksModel.createBookmark(bookmark);
     }
+  }
+
+  onSave(bookmark) {
+    this.saveBookmark(bookmark);
+    this.reset();
+  }
+
+  reset() {
+    this.currentBookmark = null;
+  }
 }
 
 export default BookmarksController;
